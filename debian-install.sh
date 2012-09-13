0e7108894a2de190cb1d93cf53ac02e5516a50b3
David Hauweele <david@hauweele.net>
2012-09-13 20:42:08 +0200

    Debian install script.

---
+++ debian-install.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# What more than a stupid sh-script could install a stupid sh-script ?
+
+if [ -f "/usr/bin/gcc.real" -o -f "/usr/bin/g++.real" ]
+then
+  echo "Already installed..."
+else
+  echo "Installing..."
+  dpkg-divert --add --rename --divert "/usr/bin/gcc.real" "/usr/bin/gcc"
+  dpkg-divert --add --rename --divert "/usr/bin/g++.real" "/usr/bin/g++"
+  cp gcc-wrapper.sh /usr/local/bin/gcc-wrapper
+  chmod a+rx /usr/local/bin/gcc-wrapper
+  ln -s /usr/local/bin/gcc-wrapper /usr/bin/gcc
+  ln -s /usr/local/bin/gcc-wrapper /usr/bin/g++
+  mkdir /etc/gcc-wrapper
+  cp default /etc/gcc-wrapper
+fi
